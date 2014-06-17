c7ec51f232c6846ad468f42de37c37269ab70521
mrchaarlie <acespade1@gmail.com>
2014-06-17 09:33:51 -0400

    load all images in the beginning

---
+++ js/loadImages.js
@@ -0,0 +1,9 @@
+
+Image1 = new Image(175,50)
+Image1.src = "img/flickr-icon-hover.png"
+
+Image2 = new Image(25,30)
+Image2.src = "img/linkedIn-icon-hover.png"
+
+Image3 = new Image(125,80)
+Image3.src = "img/behance-icon-hover.png"

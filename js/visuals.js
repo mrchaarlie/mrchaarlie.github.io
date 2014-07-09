816f3272c8a66f5d93be3586d18805623ad740f6
mrchaarlie <acespade1@gmail.com>
2014-07-09 12:30:44 -0400

    update text

---
+++ js/visuals.js
@@ -0,0 +1,25 @@
+
+Image1 = new Image(1,1);
+Image1.src = "img/flickr-icon-hover.png";
+
+Image2 = new Image(1,1);
+Image2.src = "img/linkedIn-icon-hover.png";
+
+Image3 = new Image(1,1);
+Image3.src = "img/behance-icon-hover.png";
+
+(function($){
+	/*Page Transition*/
+	$("body").css("display", "none");
+	$("body").fadeIn(800);
+	$("a.transition").click(function(event){
+		event.preventDefault();
+		linkLocation = this.href;
+		$("body").fadeOut(200, redirectPage);      
+	});
+
+	function redirectPage() {
+		window.location = linkLocation;
+	}
+	
+})(jQuery);

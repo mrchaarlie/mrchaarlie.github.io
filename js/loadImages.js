5a5c091206716cd71859bbc813cb40b0d7f37995
mrchaarlie <acespade1@gmail.com>
2014-07-24 14:47:03 -0400

    clean up

--- js/loadImages.js
+++
@@ -1,9 +0,0 @@
-
-Image1 = new Image(1,1)
-Image1.src = "img/flickr-icon-hover.png"
-
-Image2 = new Image(1,1)
-Image2.src = "img/linkedIn-icon-hover.png"
-
-Image3 = new Image(1,1)
-Image3.src = "img/behance-icon-hover.png"

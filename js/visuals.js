8dc10d82194d8d6d968258c2af8299055aaf4df0
mrchaarlie <acespade1@gmail.com>
2014-11-18 13:22:20 -0500

    slowed bg paralx

--- js/visuals.js
+++ js/visuals.js
@@ -1,18 +1,16 @@
-
 Image1 = new Image(1,1);
-Image1.src = "img/flickr-icon-hover.png";
-
+Image1.src = "img/linkedIn-icon-hover.png";
 Image2 = new Image(1,1);
-Image2.src = "img/linkedIn-icon-hover.png";
-
+Image2.src = "img/github-icon-hover.png";
 Image3 = new Image(1,1);
-Image3.src = "img/behance-icon-hover.png";
+Image3.src = "img/se-icon-hover.png";
+Image4 = new Image(1,1);
+Image4.src = "img/behance-icon-hover.png";
+Image5 = new Image(1,1);
+Image5.src = "img/flickr-icon-hover.png";
+Image6 = new Image(1,1);
+Image6.src = "img/500px-icon-hover.png";
 
-Image3 = new Image(1,1);
-Image3.src = "img/500px-icon-hover.png";
-
-Image3 = new Image(1,1);
-Image3.src = "img/github-icon-hover.png";
 
 (function($){
 	/*Page Transition*/

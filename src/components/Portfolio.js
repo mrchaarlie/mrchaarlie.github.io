10355a1cc5eb03392825b7b8157d528974e1b40e
Charlie Wu <acespade1@gmail.com>
2019-07-17 12:26:05 -0400

    Make portfolio responsive, fix last item padding

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -68,6 +68,8 @@ const portfolioData = {
   },
 }
 
+const MED_LEFT_MARGIN = 'calc((100vw - 900px + 2rem) / 2)'
+
 const OuterWrapper = styled.div`
   overflow-x: visible;
   position: relative;
@@ -77,22 +79,26 @@ const PortfolioWrapper = styled.div`
   scroll-snap-type: x proximity;
   text-align: left;
   position: relative;
-  height: 70vh;
+  height: 50vh;
   min-height: 26rem;
   margin-top: 1rem;
-  margin-left: calc((100vw - 900px + 2rem) / 2);
+  margin-left: 1rem;
   overflow-x: scroll;
   display: flex;
   background-color: ${props => props.theme.colors.lighterGrey};
-  /*border-radius: 1.125rem 0 0.5rem 0.5rem;*/
+
+  @media screen and (min-width: ${props => props.theme.screens.small}) {
+    height: 70vh;
+    margin-left: ${MED_LEFT_MARGIN};
+  }
 
   ::-webkit-scrollbar {
-    height: 1rem;
+    height: 16px;
     background-color: transparent;
   }
   ::-webkit-scrollbar-thumb {
     background: ${props => props.theme.colors.grey};
-    border-radius: 1rem;
+    border-radius: 16px;
   }
 `
 // Faux border radius because real one causes performance problems
@@ -100,9 +106,13 @@ const PortfolioBorderRadius = styled.div`
   position: absolute;
   height: ${props => `${props.size || 16}px`};
   width: ${props => `${props.size || 16}px`};
-  left: calc((100vw - 900px + 2rem) / 2);
   background: ${props => props.theme.colors.white};
   pointer-events: none;
+  left: 1rem;
+
+  @media screen and (min-width: ${props => props.theme.screens.medium}) {
+    left: ${MED_LEFT_MARGIN};
+  }
 `
 const _PortfolioBorderRadiusTL = styled(PortfolioBorderRadius)`
   top: 0;
@@ -127,21 +137,21 @@ const PortfolioBorderRadiusBL = ({ size, clipId }) => (
 )
 
 const PortfolioInnerWrapper = styled.div`
-  flex: 0 0 45rem;
   scroll-snap-align: start;
-  margin-right: 2rem;
+  flex: 0 0 90vw;
 
   &:last-of-type {
-    margin-right: calc((100vw - 900px) / 2);
+    padding-right: calc((100vw - 900px - 2rem) / 2);
+  }
 
-    &:after {
-      content: '';
-    }
+  @media screen and (min-width: ${props => props.theme.screens.medium}) {
+    flex: 0 0 45rem;
+    margin-right: 2rem;
   }
 `
 const PortfolioItem = styled.div`
   background: ${props => props.theme.colors.white};
-  border-radius: 0.75rem;
+  border-radius: 12px;
   margin: 0.5rem;
   box-shadow: 0 1px 4px rgba(0, 0, 0, 0.2);
   padding: 1rem 2rem;
@@ -150,15 +160,23 @@ const PortfolioItem = styled.div`
 `
 const PImage = styled.img`
   width: 100%;
-  height: 60%;
+  height: 40%;
   object-fit: contain;
+
+  @media screen and (min-width: ${props => props.theme.screens.small}) {
+    height: 60%;
+  }
 `
 const PName = styled.div`
   font-family: ${props => props.theme.fonts.heading};
-  font-size: 2.5rem;
+  font-size: 1.75rem;
   font-weight: 700;
   line-height: 1.15;
   color: ${props => props.theme.colors.darkGrey};
+
+  @media screen and (min-width: ${props => props.theme.screens.medium}) {
+    font-size: 2.25rem;
+  }
 `
 const PCategory = styled.div`
   font-size: 1.25rem;
@@ -187,7 +205,7 @@ const Portfolio = () => (
         ))}
       </PortfolioWrapper>
       <PortfolioBorderRadiusTL size="20" />
-      <PortfolioBorderRadiusBL size="12" />
+      <PortfolioBorderRadiusBL size="10" />
     </OuterWrapper>
   </div>
 )

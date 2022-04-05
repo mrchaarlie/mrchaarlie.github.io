29ef96642089be5bf6e220c7de552e1234ef5858
Charlie Wu <acespade1@gmail.com>
2022-04-04 22:03:39 -0700

    Add ReactGA

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import * as React from 'react'
 import { Root, Routes, addPrefetchExcludes } from 'react-static'
 import { Link } from 'components/Router'
+import ReactGA from 'react-ga'
 
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle, { theme } from './GlobalStyle'
 
 import Loading from 'components/Loading'
 import Layout from 'Layout'
+
+
+ReactGA.initialize('UA-33834473-1');
 
 const App = () => (
   <ThemeProvider theme={theme}>

d514b52eecc4a4c9d09236ac665a63cab4466033
Charlie <imcharleswu@gmail.com>
2023-08-20 09:51:52 -0700

    Change to redirect site
    Remove old site, and host a plain HTML index site to redirect

--- src/components/IAmA.js
+++
@@ -1,6 +0,0 @@
-import * as React from 'react'
-import styled from 'styled-components'
-
-const IAmA = () => <div>I am a...</div>
-
-export default IAmA
--- src/pages/loading.js
+++
@@ -1,4 +0,0 @@
-import * as React from 'react'
-import Loading from '../components/Loading'
-
-export default () => <Loading></Loading>
